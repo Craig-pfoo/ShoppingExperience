38cf65ba9bf0bc397d356f058683f6d310964215
Craig Pfau <pfaucraig@gmail.com>
2023-05-22 14:23:45 -0600

    Fixing the errors for A2-1

--- js/shop.js
+++ js/shop.js
@@ -3,19 +3,17 @@ function addProducts() {
     products.forEach(individualCard => {
        let  productCard = `
        <div class="card" id="cardNumber${individualCard.id}">
-            <h2>${individualCard.name}</h2>
-            <p>Price: $${individualCard.price}</p>
             <img src="${individualCard.image}" alt=${individualCard.description}>
-       </div>
-       <div class="cardText">
+          <div class="cardText">
             <h4>${individualCard.name}</h4>
             <p>${individualCard.description}</p>
             <p>${individualCard.price}</p>
             <button class="cartButton" id="${individualCard.id}">Add to Cart</button>
-       </div>
+           </div>
+       </div>  
    `;  
    cardContainer.innerHTML += productCard;
-    } );
+    });
 }
 addProducts();
 
