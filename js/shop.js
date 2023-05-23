21d9636b4b52e42c18db83c4b3f2811c14817938
Craig Pfau <pfaucraig@gmail.com>
2023-05-23 12:48:25 -0600

    Fixing Dollar sign

--- js/shop.js
+++ js/shop.js
@@ -7,7 +7,7 @@ function addProducts() {
           <div class="cardText">
             <h4>${individualCard.name}</h4>
             <p>${individualCard.description}</p>
-            <p>${individualCard.price}</p>
+            <p>&dollar;${individualCard.price}</p>
             <button class="cartButton" id="${individualCard.id}">Add to Cart</button>
            </div>
        </div>  
